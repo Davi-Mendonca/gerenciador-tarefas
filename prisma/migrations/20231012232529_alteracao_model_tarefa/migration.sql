455012c045cb71fe1ad7779bd0e1b2464cb29693
Davi-Mendonca <davi.mendonca@outlook.com>
2023-10-14 13:56:17 -0300

    delete prisma migrations

--- prisma/migrations/20231012232529_alteracao_model_tarefa/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `nivelPrioridade` on the `tarefas` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE `tarefas` DROP COLUMN `nivelPrioridade`,
-    ADD COLUMN `nivelPrioridade` INTEGER NOT NULL;
